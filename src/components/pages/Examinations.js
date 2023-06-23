86273a7cc5c21bfc6aad37fa388796f335646cfe
nvenkat939 <134526947+nvenkat939@users.noreply.github.com>
2023-06-24 03:40:07 +0530

    Update Examinations.js

--- src/components/pages/Examinations.js
+++ src/components/pages/Examinations.js
@@ -1,6 +1,139 @@
 import React from 'react';
-import '../../App.css';
+import '../../components/pages/Examinations.css';
+import ImageSrc from '../../components/pages/jee.jpeg';
+import ImagSrc from '../../components/pages/advance.jpeg';
+import ImaeSrc from '../../components/pages/ts.jpeg';
+import ImgeSrc from '../../components/pages/ap.jpeg';
+import ImeSrc from '../../components/pages/bitsat.jpeg';
+//import React from 'react';
 
-export default function Examinations(){
-    return <h1 className='Examinations'>EXAMINATIONS</h1>;
+function Examinations() {
+  return (
+    <div>
+      <h1>EXAMINATIONS</h1>
+
+
+      <h2>JEE MAINS(JOINT ENTRANCE EXAMINATIONS)</h2>
+      <img src={ImageSrc} alt="cse" />
+      <p>
+      JEE (Joint Entrance Examination) is a national-level engineering entrance exam conducted in India for admission to undergraduate engineering programs offered by various IITs, NITs, IIITs, and other prestigious engineering colleges in India. The exam is conducted in two stages - JEE Main and JEE Advanced.The JEE is of utmost importance for students who wish to pursue undergraduate engineering or architecture programs in India. The exam is conducted at a national level and is considered as one of the most competitive entrance exams in India. The JEE score is used by top engineering colleges in India to shortlist candidates for admission to their undergraduate programs.
+
+Apart from IITs and NITs, many other prestigious engineering institutes in India, such as BITS Pilani, Delhi Technological University, VIT University, and many more, consider JEE scores for admission to their undergraduate engineering programs.
+
+The JEE score is also a crucial factor in determining the eligibility of students for scholarships and other financial aids. Hence, scoring well in the JEE is essential for students who wish to pursue a career in engineering or architecture in India.
+
+In conclusion, JEE is an important entrance exam for students who aspire to pursue undergraduate engineering or architecture programs in India. It is a highly competitive exam that requires thorough preparation and hard work to crack. Scoring well in JEE opens up numerous opportunities for students to pursue their dream career in the field of engineering or architecture.Mathematics: 25 (20+10) 10 Questions with answers as a numerical value. Out of 10 questions, 5 questions are compulsory.
+
+Physics: 25 (20+10) 10 Questions with answers as a numerical value. Out of 10 questions, 5 questions are compulsory.
+
+Chemistry: 25 (20+10) 10 Questions with answers as a numerical value. Out of 10 questions, 5 questions are compulsory.
+
+Total: 75 Questions (25 questions each)JEE Mains total marks
+
+300 Marks (100 marks for each section)
+      </p>
+
+      <h2>JEE ADVANCED</h2>
+      <img src={ImagSrc} alt="advanced" />
+      <p>
+      Joint Entrance Examination – Advanced (JEE-Advanced) (formerly the Indian Institute of Technology - Joint Entrance Examination (IIT-JEE)), is an academic examination held annually in India. It is organised by one of the seven zonal IITs (IIT Roorkee, IIT Kharagpur, IIT Delhi, IIT Kanpur, IIT Bombay, IIT Madras, and IIT Guwahati) under the guidance of the Joint Admission Board (JAB) on a round-robin rotation pattern for the qualifying candidates of the JEE-Main (exempted for Foreign Candidates). It used to be the sole prerequisite for admission to the Indian Institutes of Technology bachelor's programs before the introduction of UCEED, Online B.S. and Olympiad entries, but seats through these new mediums are very low or difficult to complete the course.
+      JEE(Advanced) is conducted in two shifts of three hours each - Paper-1 and Paper-2, both the papers consist questions from three major subjects- Physics, Chemistry and Mathematics. Unlike most of the other exams, the type and the number of questions being asked in the paper varies from year to year, with an average of about 36-38 questions asked from each subject across both the shifts, For example, the 2021 JEE Advanced paper had 38 questions (19 questions in Paper-1 and the next 19 in Paper-2) from each of the three subjects.
+
+Every subject had questions divided into 4 main sections-
+1. Section 1 consisted of 4 Single correct-multiple choice questions with marking scheme of +4 for every correct answer, 0 if question is left unanswered and -1 for every wrong answer.
+
+2. Section 2 consisted of Three question stems with 2 questions per stem, each of which had a numerical answer with two decimal places. Marking scheme for the questions was +2 for every correct answer and 0 in all other cases.
+
+3. Section 3 consisted of 6 Multi correct- multiple choice questions, where one or more than one options for each question were correct. It had a marking scheme of +4 If only (all) the correct option(s) is(are) chosen; +3 If all the four options are correct but only three options are chosen; +2 If three or more options are correct but only two options are chosen, both of which are correct; +1 If two or more options are correct but only one option is chosen and it is a correct option; 0 If unanswered; while a −2 was given for every wrong one chosen.
+
+4. Section 4 consisted of 3 questions whose answers where ‘non-negative integers’, every question carried a +4 for correct integer filled and 0 for all the other cases.
+
+
+    </p>
+      
+    <h2>TS EAMCET</h2>
+      <img src={ImaeSrc} alt="ts" />
+
+      <p>
+      Telangana State Engineering, Agriculture and Medical Common Entrance Test (TS EAMCET) is a state-level entrance test for admission to BE, BTech, BPharm, PharmD, BSc, BFSc, and BVSc courses. This state-level examination is the prerequisite for admission into various professional courses offered in University / Private Colleges in Telangana and is held once a year. Candidates are allotted seats based on the TS EAMCET marks and rank via an online centralized counselling process. To get a place in the  TS EAMCET rank list, candidates have to score at least 25% of the maximum marks in TS EAMCET. However, no minimum qualifying mark is prescribed for candidates belonging to SC / ST candidates.
+      TS EAMCET is for both stream of students, total 160 marks are present.For MPC students maths-80 marks,chemistry 40 marks and physics 40 marks.And for Bipc students Botany - 40 marks,Zoology - 40 marks,Physics - 40 marks and Chemistry - 40 marks.
+      </p>
+     
+      <h2>AP EAMCET</h2>
+      <img src={ImgeSrc} alt="ts" />
+
+      <p>
+      AP EAMCET is a common entrance test conducted by Jawaharlal Nehru Technological University, Kakinada, on behalf of the Andhra Pradesh State Council of Higher Education (APSCHE).
+      AP EAMCET (Andhra Pradesh Engineering Agricultural and Medical Common Entrance Test) has been renamed AP EAPCET, which stands for Andhra Pradesh Engineering Agricultural and Pharmacy Common Entrance Test. AP EAMCET is an entrance examination conducted by the Jawaharlal Nehru Technological University (JNTU), Kakinada on behalf of the Andhra Pradesh State Council of Higher Education (APSCHE) for admission into various professional courses offered in University/ Private Colleges in the state of Andhra Pradesh. The exam tests the candidate's knowledge and aptitude in various subjects such as physics, chemistry, biology, and mathematics. It is typically held in May, and the results are used to determine the eligibility of candidates for admission to engineering, agriculture, and pharmacy programmes in colleges and universities in Andhra Pradesh.
+      </p>
+
+      <h2>BITSAT</h2>
+      <img src={ImeSrc} alt="ts" />
+
+      <p>
+      Birla Institute of Technology and Science Admission Test (BITSAT) 2023 is an entrance exam conducted by the Birla Institute of Technology and Science (BITS), Pilani, for admissions to undergraduate engineering courses (BE) at its three campuses located at Pilani, Goa and Hyderabad.Exam Level
+
+University-based National Level Exam,Exam Level
+
+University-based National Level Exam
+
+Mode of Application
+
+Online
+
+Mode of exam
+
+Computer-based Test
+
+Application Fee
+
+Boys: INR 3,400/- (1st Session) INR 5,400/- (Both Sessions)
+
+Girls: INR 2,900/- (1st Session) INR 4,400/- (Both Sessions)
+
+Exam Frequency
+
+Twice a year
+
+Exam Duration
+
+3 hours
+
+Number of Questions
+
+130 MCQs
+
+Total Seats Offered
+
+Around 2,000
+
+Applications Per Year
+
+About 3 Lakh
+
+Number of Exam Centres
+
+61
+
+Accepting Colleges
+
+BITS Pilani, BITS Hyderabad, BITS Goa
+Some previous year papers also included matrix match type questions instead of Single Correct - multiple choice questions.
+Exam Mode: BITSAT 2023 will be conducted online (computer-based) only.
+No. of questions: There are a total of 130 questions in the exam paper.
+Medium: The question paper will be in the English language.
+Sections: The question paper will consist of 4 sections/parts.
+Time Duration: Candidates have to attempt the question paper within 3 hours i.e. 180 minutes.
+Marking scheme: 3 marks for each correct answer and 1 mark will be deducted for each incorrect answer.
+    
+      </p>
+
+
+
+
+
+    </div>
+  );
 }
+
+export default Examinations;
